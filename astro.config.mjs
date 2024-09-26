2ca132f68a15a97ce94eb9e6ca9d000b1b9af39e
Dharma Bob <182971306+dharmabob-ai@users.noreply.github.com>
2024-09-26 17:27:57 -0400

    import template

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    viewTransitions: true,
+  },
+  integrations: [tailwind(), compress()],
+  site: 'https://dharmabob-ai.github.io'
+});
